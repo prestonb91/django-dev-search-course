b0b841c1a276b1a63bd594eaa6f663c2e23d6485
prestonb91 <prestonb91@gmail.com>
2025-02-11 10:41:51 +0900

    feat: implement password reset urls and templates

--- dev-search/settings.py
+++ dev-search/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os 
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -130,8 +131,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = env(EMAIL_HOST_USER)
-EMAIL_HOST_PASSWORD = env(EMAIL_HOST_PASSWORD)
+# EMAIL_HOST_USER = env(EMAIL_HOST_USER)
+# EMAIL_HOST_PASSWORD = env(EMAIL_HOST_PASSWORD)
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
